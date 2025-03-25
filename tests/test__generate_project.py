ef7982ead9e82990432d08fccbd974c5c6a39f44
divakaivan <timee98@outlook.com>
2025-03-25 21:01:17 +0900

    extract away the generate_project to utils

--- tests/test__generate_project.py
+++ tests/test__generate_project.py
@@ -1,13 +1,9 @@
-import json
 import shutil
-import subprocess
-from copy import deepcopy
 from pathlib import Path
 
 import pytest
 
-THIS_DIR = Path(__file__).parent
-PROJECT_DIR = (THIS_DIR / "../").resolve()
+from tests.utils.project import generate_project
 
 
 @pytest.fixture(scope="function")
@@ -18,27 +14,6 @@ def project_dir() -> Path:
     generated_repo_dir: Path = generate_project(template_values=template_values)
     yield generated_repo_dir
     shutil.rmtree(generated_repo_dir)
-
-
-def generate_project(template_values: dict[str, str]):
-    template_values = deepcopy(template_values)
-    cookiecutter_config = {"default_context": template_values}
-    cookiecutter_config_fpath = PROJECT_DIR / "cookiecutter-test-config.json"
-    cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
-    subprocess.run(
-        [
-            "cookiecutter",
-            str(PROJECT_DIR),
-            "--output-dir",
-            str(PROJECT_DIR / "sample"),
-            "--no-input",
-            "--config-file",
-            str(cookiecutter_config_fpath),
-        ],
-        check=True,
-    )
-    generated_repo_dir = PROJECT_DIR / "sample" / template_values["repo_name"]
-    return generated_repo_dir
 
 
 def test__can_generate_project(project_dir: Path):
