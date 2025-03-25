f2259010a9fa591ceb4840bcca1fed1866ce1918
divakaivan <timee98@outlook.com>
2025-03-26 00:32:45 +0900

    add comments for linting

---
+++ {{cookiecutter.repo_name}}/tests/fixtures/example_fixture.py
@@ -0,0 +1,14 @@
+"""Example fixture for tests."""
+
+from uuid import uuid4
+
+import pytest
+
+from tests.consts import PROJECT_DIR
+
+
+@pytest.fixture(scope="session")
+def test_session_id() -> str:
+    """Generate a unique test session ID."""
+    test_session_id = str(PROJECT_DIR) + str(uuid4())[:6]
+    return test_session_id
