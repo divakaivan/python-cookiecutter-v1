2a36cdc4332ce15150c743d5dbf6600b6a072dd5
divakaivan <timee98@outlook.com>
2025-03-25 20:08:04 +0900

    add 1st test for creating a sample repo from template

---
+++ tests/test__generate_project.py
@@ -0,0 +1,34 @@
+import json
+import subprocess
+from pathlib import Path
+
+THIS_DIR = Path(__file__).parent
+PROJECT_DIR = (THIS_DIR / "../").resolve()
+
+
+def test__can_generate_project():
+    """
+    execute: `cookiecutter <template_dir> ...`
+    """
+    cookiecutter_config = {
+        "default_context": {
+            "repo_name": "test-repo",
+        }
+    }
+    cookiecutter_config_fpath = PROJECT_DIR / "cookiecutter-test-config.json"
+    cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
+    subprocess.run(
+        [
+            "cookiecutter",
+            str(PROJECT_DIR),
+            "--output-dir",
+            str(PROJECT_DIR / "sample"),
+            "--no-input",
+            "--config-file",
+            str(cookiecutter_config_fpath),
+        ],
+        check=True,
+    )
+
+    generated_project_dir = PROJECT_DIR / "sample" / cookiecutter_config["default_context"]["repo_name"]
+    assert generated_project_dir.exists()
