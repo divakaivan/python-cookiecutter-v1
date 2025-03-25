85fa4a17a43027c1bb43b8869fcf9762a03e49c3
divakaivan <timee98@outlook.com>
2025-03-26 00:05:01 +0900

    add test for make test

--- tests/test__makefile.py
+++ tests/test__makefile.py
@@ -12,14 +12,12 @@ def project():
 
 
 def test__linting_passes(project_dir: Path):
-    subprocess.run(
-        ["make", "lint-ci"],
-        cwd=project_dir,
-        check=True,
-    )
+    subprocess.run(["make", "lint-ci"], cwd=project_dir, check=True)
 
 
-def test_tests_pass(project): ...
+def test__tests_pass(project_dir: Path):
+    subprocess.run(["make", "install"], cwd=project_dir, check=True)
+    subprocess.run(["make", "test-wheel-locally"], cwd=project_dir, check=True)
 
 
 def test__install_succeeds(project): ...
